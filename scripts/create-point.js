83fa453b33fc7acfaacbf8363ec05712436c01c0
Djoni Bourscheid <djonibourscheid00@gmail.com>
2020-06-03 12:06:28 -0300

    Correção do form
    - Resetando a lista de cidades quando um estado novo é selecionado
    
    - Todos os forms agora são obrigatórios antes de enviar

--- scripts/create-point.js
+++ scripts/create-point.js
@@ -21,11 +21,19 @@ function getCities(event) {
 
     const url = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${ufValue}/municipios`
 
+    // Para resetar a lista de cidades quando selecionado
+    // um novo estado e deixando o campo bloqueado de novo (sera desativado o bloqueio quando a api será chamada novamente)
+
+    citySelect.innerHTML = `<option value>Selecione a cidade</option>`
+    citySelect.disabled = true
+
     fetch(url)
     .then( res => res.json() ) // função anonima, escrita de forma curta
     .then( cities => {
+
+        // colocando as cidades
         for (const city of cities) {
-            citySelect.innerHTML += `<option value="${city.id}">${city.nome}</option>`
+            citySelect.innerHTML += `<option value="${city.nome}">${city.nome}</option>`
         }
 
         citySelect.disabled = false
